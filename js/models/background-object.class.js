b36dd700c78881ef4e267f2f9f3c5affd4b3cb08
Onur Bayram <onur-bayram@hotmail.de>
2026-02-13 13:44:38 +0100

    Update background object

--- js/models/background-object.class.js
+++ js/models/background-object.class.js
@@ -1,12 +1,11 @@
 class BackgroundObject extends MovableObject {
-
     constructor(imagePath, x, y) {  
         super();
         this.loadImage(imagePath);
         this.x = x;
         this.y = y;
         this.width = 720;
-        this.height = 480;
+        this.height = 300;
 
 
     }
