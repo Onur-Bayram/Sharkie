447fea67dc6ff87ccec25eb013d5fe5ffc884ddc
Onur Bayram <onur-bayram@hotmail.de>
2026-02-13 13:31:55 +0100

    Draw backgrounds and set dimensions

---
+++ js/models/background-object.class.js
@@ -0,0 +1,14 @@
+class BackgroundObject extends MovableObject {
+
+    constructor(imagePath, x, y) {  
+        super();
+        this.loadImage(imagePath);
+        this.x = x;
+        this.y = y;
+        this.width = 720;
+        this.height = 480;
+
+
+    }
+
+}
--- js/models/world.class.js
+++ js/models/world.class.js
@@ -29,6 +29,9 @@ constructor(canvas) {
     draw() {
             this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
 
+    this.backgrounds.forEach((background) => {
+        this.ctx.drawImage(background.img, background.x, background.y, background.width, background.height);
+    });
     this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
     this.enemies.forEach((enemy) => {
         this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
