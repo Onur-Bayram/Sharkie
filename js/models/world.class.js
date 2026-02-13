f3086031697cc01d2f262c9d6afee28eaa21fccb
Onur Bayram <onur-bayram@hotmail.de>
2026-02-13 14:07:07 +0100

    Fix background layer filenames

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -9,8 +9,8 @@ class World {
 
 backgroundObjects = [
     new BackgroundObject('3. Background/Layers/1. Light/1.png', 0, 0),
-    new BackgroundObject('3. Background/Layers/2. Floor/1.png', 0, 0),
-    new BackgroundObject('3. Background/Layers/5. Water/1.png', 0, 0),
+    new BackgroundObject('3. Background/Layers/2. Floor/L.png', 0, 0),
+    new BackgroundObject('3. Background/Layers/5. Water/L.png', 0, 0),
 ];  
 canvas; 
 ctx; 
