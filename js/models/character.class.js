b196ec6a82c9015ace9cd5123ae21a0b32041ac4
Onur Bayram <onur-bayram@hotmail.de>
2026-02-13 12:42:43 +0100

    Fix asset paths for character and puffer fish

---
+++ js/models/character.class.js
@@ -0,0 +1,16 @@
+class Character extends MovableObject{
+
+    
+    constructor() {
+        super(this.loadImage('1.Sharkie/1.IDLE/1.png'));
+
+
+    }
+
+
+    jump() {
+
+    }
+
+
+}
---
+++ js/models/puffer-fish.class.js
@@ -0,0 +1,9 @@
+class Pufferfish extends MovableObject{
+
+
+    constructor(){
+        super();
+        this.loadImage('2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
+    }
+
+}
