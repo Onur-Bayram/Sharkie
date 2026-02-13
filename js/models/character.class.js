21113ceb743a8e4ce02974c75ad9ed5da7fe2e34
Onur Bayram <onur-bayram@hotmail.de>
2026-02-13 15:31:16 +0100

    Add character animation system with IDLE animation

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -1,15 +1,42 @@
 class Character extends MovableObject{
 
+    IMAGES_IDLE = [
+        '1.Sharkie/1.IDLE/1.png',
+        '1.Sharkie/1.IDLE/2.png',
+        '1.Sharkie/1.IDLE/3.png',
+        '1.Sharkie/1.IDLE/4.png',
+        '1.Sharkie/1.IDLE/5.png',
+        '1.Sharkie/1.IDLE/6.png',
+        '1.Sharkie/1.IDLE/7.png',
+        '1.Sharkie/1.IDLE/8.png',
+        '1.Sharkie/1.IDLE/9.png',
+        '1.Sharkie/1.IDLE/10.png',
+        '1.Sharkie/1.IDLE/11.png',
+        '1.Sharkie/1.IDLE/12.png',
+        '1.Sharkie/1.IDLE/13.png',
+        '1.Sharkie/1.IDLE/14.png',
+        '1.Sharkie/1.IDLE/15.png',
+        '1.Sharkie/1.IDLE/16.png',
+        '1.Sharkie/1.IDLE/17.png',
+        '1.Sharkie/1.IDLE/18.png'
+    ];
     
     constructor() {
         super();
         this.loadImage('1.Sharkie/1.IDLE/1.png');
+        this.loadImages(this.IMAGES_IDLE);
         this.width = 200;
         this.height = 140;
-
-
+        this.animate();
     }
 
+    animate() {
+        setInterval(() => {
+            let path = this.IMAGES_IDLE[this.currentImage % this.IMAGES_IDLE.length];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
+    }
 
     jump() {
 
--- js/models/moveable-object.class.js
+++ js/models/moveable-object.class.js
@@ -4,12 +4,21 @@ y = 250;
 img;
 height = 150;
 width = 100;
-
+imageCache = {};
+currentImage = 0;
 
 
 loadImage(path) {
     this.img = new Image();
     this.img.src = encodeURI(path);
+}
+
+loadImages(arr) {
+    arr.forEach(path => {
+        let img = new Image();
+        img.src = encodeURI(path);
+        this.imageCache[path] = img;
+    });
 }
 
 
