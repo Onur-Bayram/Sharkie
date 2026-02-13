1610043587b9f7f7f783c82fbaf6849bb36508c1
Onur Bayram <onur-bayram@hotmail.de>
2026-02-13 13:56:51 +0100

    Fix image loading check

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -8,9 +8,9 @@ class World {
 ];
 
 backgroundObjects = [
-    new BackgroundObject('3. Background/Layers/1. Light/1.png', 0, 0),
-    new BackgroundObject('3. Background/Layers/2. Floor/1.png', 0, 0),
-    new BackgroundObject('3. Background/Layers/5. Water/1.png', 0, 0),
+    new BackgroundObject('3.%20Background/Layers/1.%20Light/1.png', 0, 0),
+    new BackgroundObject('3.%20Background/Layers/2.%20Floor/1.png', 0, 0),
+    new BackgroundObject('3.%20Background/Layers/5.%20Water/1.png', 0, 0),
 ];  
 canvas; 
 ctx; 
@@ -26,15 +26,15 @@ draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     this.backgroundObjects.forEach((background) => {
-        if (background.img) {
+        if (background.img && background.img.complete && background.img.naturalHeight !== 0) {
             this.ctx.drawImage(background.img, background.x, background.y, background.width, background.height);
         }
     });
-    if (this.character.img) {
+    if (this.character.img && this.character.img.complete && this.character.img.naturalHeight !== 0) {
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
     }
     this.enemies.forEach((enemy) => {
-        if (enemy.img) {
+        if (enemy.img && enemy.img.complete && enemy.img.naturalHeight !== 0) {
             this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
         }
     }); 
